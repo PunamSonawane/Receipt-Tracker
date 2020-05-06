fd7041a05a98079bab0bd37b960d72df01813bd3
PunamSonawane <sonawanepunam27@gmail.com>
2020-05-06 09:51:01 -0400

    Update createReceiptVision_handler.py

--- api/createReceiptVision_handler.py
+++ api/createReceiptVision_handler.py
@@ -37,7 +37,7 @@ def createReceiptVision():
         # Google cloud vision API for text detection in image receipts
         print("google credential--------")
         credi=os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-        person_dict = json.loads(credi)
+        person_dict = json.load(credi)
         print(person_dict)
         print(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
 
